17c54da6dfedb83cbbae682371734ae3506e05b8
Aymeric <aymeric.bouault@gmail.com>
2023-06-11 17:15:14 +0200

    modif

--- convertisseur-salaire/src/components/Convertisseur.jsx
+++ convertisseur-salaire/src/components/Convertisseur.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import SalaireMensuel from './SalaireMensuel';
 import SalaireAnnuel from './SalaireAnnuel';
-import Simulateur from './Simulateur';
 
 const Convertisseur = () => {
     return (
         <div>
             <SalaireMensuel />
             <SalaireAnnuel />
-            <Simulateur />
         </div>
     );
 };
